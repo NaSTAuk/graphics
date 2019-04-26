42b6647a4eed7d40131f7136802bbaac093eb99c
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-04-26 20:21:40 +0100

    add cjs to gitignore

--- packages/framework/core/cjs/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphics_logger_1 = require("@nastauk/graphics-logger");
-graphics_logger_1.logError('error');
-graphics_logger_1.logInfo('log');
-graphics_logger_1.logWarn('warn');
-//# sourceMappingURL=index.js.map
--- packages/framework/logger/cjs/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare const logError: (error: string) => void;
-declare const logInfo: (value: string) => void;
-declare const logWarn: (value: string) => void;
-export { logError, logInfo, logWarn };
-//# sourceMappingURL=index.d.ts.map
