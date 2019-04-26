25a86e6c48d777f4dbcc8b38b4fdf16e21ab1526
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-04-26 21:18:42 +0100

    add jest

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverageFrom: [
+    '!**/cjf/**',
+    'packages/framework/**/*.{ts,tsx}',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/docs/', '/packages/*/*/cjs/'],
+  coverageReporters: ['html', 'text', 'json'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  watchPathIgnorePatterns: ['/node_modules/', '/docs/', '/packages/*/*/cjs/'],
+  "roots": [
+    "./packages",
+  ],
+  "transform": {
+    "^.+\\.ts$": "ts-jest",
+  },
+  testRegex: '\\.test\\.ts?$',
+  "moduleFileExtensions": [ "ts", "js" ],
+}
---
+++ packages/framework/logger/test/index.test.ts
@@ -0,0 +1,5 @@
+describe('server', () => {
+    it('works', () => {
+        expect(true).toEqual(true)
+    })
+})
