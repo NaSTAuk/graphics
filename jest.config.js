9f294cf7006e73f0b955a50fe9793b2eea50f9a6
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-05-09 22:02:38 +0100

    simplific jest config from tutorial excessivness

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   clearMocks: true,
   collectCoverageFrom: [
-    '!**/cjf/**',
     '!packages/**/dist/*.{ts,tsx}',
     'packages/**/*.{ts,tsx}',
   ],
-  coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: ['/node_modules/', '/docs/', '/packages/*/*/dist/'],
   coverageReporters: ['html', 'text', 'json', 'lcov'],
   coverageThreshold: {
@@ -16,23 +14,7 @@ module.exports = {
       statements: 100
     }
   },
-  globals: {
-    'ts-jest': {
-      extends: './babel.config.js',
-    },
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js'],
-  notify: true,
-  notifyMode: 'always',
-  roots: [
-    './packages',
-  ],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
   testRegex: '\\.test\\.ts?$',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  watchPathIgnorePatterns: ['/node_modules/', '/docs/', '/packages/*/*/cjs/'],
+  watchPathIgnorePatterns: ['/node_modules/', '/docs/', '/packages/*/*/dist/'],
 }
