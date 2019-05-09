a3a29cf0889e0e1a0fdf7d209c722f89c61a3ac1
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-05-09 20:31:14 +0100

    add stylelint and prettier

--- packages/framework/logger/test/index.test.ts
+++ packages/framework/logger/test/index.test.ts
@@ -1,28 +1,28 @@
-import {logError, logInfo, logWarn} from '../src'
+import { logError, logInfo, logWarn } from '../src'
 
 describe('server', () => {
-    it('works', () => {
-        expect(true).toEqual(true)
-    })
+  it('works', () => {
+    expect(true).toEqual(true)
+  })
 })
 
 /* tslint:disable:no-console */
 test('should wrap console', () => {
-    console.error = jest.fn()
-    console.info = jest.fn()
-    console.warn = jest.fn()
+  console.error = jest.fn()
+  console.info = jest.fn()
+  console.warn = jest.fn()
 
-    logError('error')
-    logInfo('info')
-    logWarn('warn')
+  logError('error')
+  logInfo('info')
+  logWarn('warn')
 
-    expect(console.error).toHaveBeenCalledTimes(1)
-    expect(console.error).toHaveBeenCalledWith('error')
+  expect(console.error).toHaveBeenCalledTimes(1)
+  expect(console.error).toHaveBeenCalledWith('error')
 
-    expect(console.info).toHaveBeenCalledTimes(1)
-    expect(console.info).toHaveBeenCalledWith('info')
+  expect(console.info).toHaveBeenCalledTimes(1)
+  expect(console.info).toHaveBeenCalledWith('info')
 
-    expect(console.warn).toHaveBeenCalledTimes(1)
-    expect(console.warn).toHaveBeenCalledWith('warn')
+  expect(console.warn).toHaveBeenCalledTimes(1)
+  expect(console.warn).toHaveBeenCalledWith('warn')
 })
 /* tslint:disable:no-console */
