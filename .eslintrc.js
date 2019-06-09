621fc56ae71d3121bb9608e182568cc4298ca29e
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-06-09 09:27:14 +0100

    make disabling work

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['prettier', 'standard-with-typescript'],
-  parser: '@typescript-eslint/parser',
+  extends: ['standard-with-typescript', 'prettier'],
   parserOptions: {
       project: './tsconfig.json'
   },
-  rules: {
-    'comma-dangle': ['error', 'only-multiline'],
-    '@typescript-eslint/prefer-interface': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-  },
+  plugins: ["@typescript-eslint"],
+  overrides: [{
+    files: ['*.ts', '*.js'],
+    rules: {
+      'comma-dangle': ['error', 'only-multiline'],
+      '@typescript-eslint/prefer-interface': 0,
+      '@typescript-eslint/explicit-function-return-type': 0
+    }
+  }],
 }
 
